57bcc573fa87bb0e0ec24ac3606a5ef16bce2f82
Jakub Królicki <101356174+Krolicki@users.noreply.github.com>
2022-07-03 13:18:13 +0200

    Animate sections

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector(".menu");
 const close = document.querySelector(".close");
 const nav = document.querySelector("nav");
+const slideElements = document.querySelectorAll(".more-row");
 
 menu.addEventListener("click", () => {
     nav.classList.add("open-nav");
@@ -9,3 +10,38 @@ menu.addEventListener("click", () => {
 close.addEventListener("click", () => {
     nav.classList.remove("open-nav");
 })
+
+function debounce(func, wait = 17, immediate = true) {
+    var timeout;
+  
+    return function executedFunction() {
+      var context = this;
+      var args = arguments;
+          
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+  
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait); 
+
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+function slide(e){
+
+    slideElements.forEach(element => {
+        const scrollDist = (window.scrollY + window.innerHeight).toFixed() - element.clientHeight / 2;
+        //const elementDist = element.offsetTop + element.clientHeight;
+        if(scrollDist > element.offsetTop)
+            element.classList.add("slideIn");
+        if(scrollDist < element.offsetTop)
+            element.classList.remove("slideIn");
+    });
+}
+
+
+window.addEventListener('scroll', debounce(slide));
