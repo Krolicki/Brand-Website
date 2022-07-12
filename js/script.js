b5b19c4753d57a6b3bf08c3d09836233dc47e3b4
Jakub Królicki <101356174+Krolicki@users.noreply.github.com>
2022-07-12 15:52:54 +0200

    Code repair

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ const listItems = document.querySelectorAll("li");
 const rates = document.querySelectorAll(".rate");
 const seeMoreButtons = document.querySelectorAll(".new-dsc button");
 
+var lastScroll = 0;
+
 menu.addEventListener("click", () => {
     nav.classList.add("open-nav");
 })
@@ -14,7 +16,7 @@ close.addEventListener("click", () => {
     nav.classList.remove("open-nav");
 })
 
-function debounce(func, wait = 17, immediate = true) {
+function debounce(func, wait = 12, immediate = true) {
     var timeout;
   
     return function executedFunction() {
@@ -55,6 +57,21 @@ function slide(e){
         if(scrollDist <  getElemDistance(element))
             element.classList.remove("slideIn");
     });
+    
+    let scroll = window.pageYOffset;
+
+    if(scroll <= 0){
+        document.body.classList.remove("scroll-up");
+    }
+    if(scroll > lastScroll && !document.body.classList.contains("scroll-down")){
+        document.body.classList.add("scroll-down");
+        document.body.classList.remove("scroll-up");
+    }
+    if(scroll < lastScroll && document.body.classList.contains("scroll-down")){
+        document.body.classList.add("scroll-up");
+        document.body.classList.remove("scroll-down");
+    }
+    lastScroll = scroll;
 }
 
 window.addEventListener('scroll', debounce(slide));
